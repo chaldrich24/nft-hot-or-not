01df2d98ac236dd6f0ee62fa8e18f808a95d0673
chaldrich24 <chaldrich24@gmail.com>
2022-02-03 15:48:13 -0500

    add styling to comment and various elements

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -6,79 +6,79 @@ import { Link } from 'react-router-dom'
 
 
 const Login = (props) => {
-    const [formState, setFormState] = useState({ email: '', password: '' });
-  
-    const [login, { error }] = useMutation(LOGIN_USER);
-  
-    // update state based on form input changes
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-  
-      setFormState({
-        ...formState,
-        [name]: value,
-      });
-    };
-  
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const [login, { error }] = useMutation(LOGIN_USER);
+
+  // update state based on form input changes
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
   // submit form
   const handleFormSubmit = async event => {
     event.preventDefault();
-  
+
     try {
       const { data } = await login({
         variables: { ...formState }
       });
-    
+
       Auth.login(data.login.token);
     } catch (e) {
       console.error(e);
     }
   };
-  
-    return (
-      <main className='flex-row justify-center mb-4'>
-        <div className='col-12 col-md-6'>
-          <div className='card'>
-            <h4 className='card-header'>Login</h4>
-            <div className='card-body'>
-              <form onSubmit={handleFormSubmit}>
-                <input 
-                  className='form-input m-4 w-25 p-2 justify-center'
-                  placeholder='Your email'
-                  name='email'
-                  type='email'
-                  id='email'
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <br></br>
-                <input
-                  className='form-input  w-25 p-2'
-                  placeholder='******'
-                  name='password'
-                  type='password'
-                  id='password'
-                  value={formState.password}
-                  onChange={handleChange}
-                />
+
+  return (
+    <main className='flex-row justify-center mb-4'>
+      <div className='col-12 col-md-6'>
+        <div className='card'>
+          <h4 className='card-header'>Login to begin voting!</h4>
+          <div className='card-body'>
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className='form-input m-4 w-25 p-2 justify-center'
+                placeholder='Email'
+                name='email'
+                type='email'
+                id='email'
+                value={formState.email}
+                onChange={handleChange}
+              />
               <br></br>
-              <button  type='submit'>
-                  Submit
-                </button>
-              </form>
+              <input
+                className='form-input  w-25 p-2'
+                placeholder='*********'
+                name='password'
+                type='password'
+                id='password'
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <br></br>
+              <button type='submit'>
+                Submit
+              </button>
+            </form>
 
 
-                <br></br>
-                <Link to="/signup">
-                    Click Here to Create an Account
-                </Link>
+            <br></br>
+            <Link className='create-account' to="/signup">
+              Click Here to Create an Account
+            </Link>
 
-              {error && <div>Login failed</div>}
-            </div>
+            {error && <div>Login failed</div>}
           </div>
         </div>
-      </main>
-    );
-  };
-  
-  export default Login;
+      </div>
+    </main>
+  );
+};
+
+export default Login;
